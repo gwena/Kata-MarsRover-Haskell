42a9911887e8aaa9a327315020e357816843375e
Gwenael Ropert <gwena.code@gmail.com>
2017-12-01 13:21:21 +0200

    Feature - Move rover based on cardinal point

--- MarsRover.hs
+++ MarsRover.hs
@@ -3,6 +3,16 @@ module MarsRover where
 import MarsRoverTypes
 import Parser
 
+move :: Position -> CardinalPoint -> Position
+move (x, y) cp = (x + x', y + y')
+   where (x', y') = offset cp
+
+offset :: CardinalPoint -> (Int, Int)
+offset N = (0,  1)
+offset S = (0, -1)
+offset E = (1,  0)
+offset W = (-1, 0)
+
 process :: String -> String
 process str = case parseInput str of
                 Left e -> "ERROR: " ++ e
--- Spec.hs
+++ Spec.hs
@@ -7,11 +7,18 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "parse input correctly" $ do
-    it "return Parsed message if input was valid " $ do
+    it "return Parsed message if input was valid" $ do
       let test = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM\n"
       process test `shouldBe` "Parsed"
 
     it "return Error message if input was invalid " $ do
       let test = "5 5a\n1 giberish\n"
       process test `shouldStartWith` "ERROR: "
+
+  describe "move rover to correct position" $ do
+    it "add 1 on the y coordinate when going North" $ do
+      move (2,2) N `shouldBe` (2,3)
+
+    it "substract 1 on the x coordinate when going West" $ do
+      move (2,2) W `shouldBe` (1,2)
 
