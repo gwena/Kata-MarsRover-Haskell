4d92469d09adc68a4f2e6ebf3ff8983f70fe8b44
Gwenael Ropert <gwena.code@gmail.com>
2017-12-01 13:08:17 +0200

    Test - Check for invalid Input to parser

--- Spec.hs
+++ Spec.hs
@@ -11,3 +11,7 @@ main = hspec $ do
       let test = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM\n"
       process test `shouldBe` "Parsed"
 
+    it "return Error message if input was invalid " $ do
+      let test = "5 5a\n1 giberish\n"
+      process test `shouldStartWith` "ERROR: "
+
